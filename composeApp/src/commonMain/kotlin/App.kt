5a1619c8c777295a10abf752c3a96294f9c3a95c
Rony <rony.c2015@gmail.com>
2024-01-27 22:41:19 -0500

    add camera and photo settings

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,6 +1,8 @@
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -9,26 +11,138 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
+import shared.PermissionCallback
+import shared.PermissionStatus
+import shared.PermissionType
+import shared.createPermissionsManager
+import shared.rememberCameraManager
+import shared.rememberGalleryManager
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        val greeting = remember { Greeting().greet() }
+        val coroutineScope = rememberCoroutineScope()
+        var imageBitmap by remember { mutableStateOf<ImageBitmap?>(null) }
+        var imageSourceOptionDialog by remember { mutableStateOf(value = false) }
+        var launchCamera by remember { mutableStateOf(value = false) }
+        var launchGallery by remember { mutableStateOf(value = false) }
+        var launchSetting by remember { mutableStateOf(value = false) }
+        var permissionRationalDialog by remember { mutableStateOf(value = false) }
+        val permissionsManager = createPermissionsManager(object : PermissionCallback {
+            override fun onPermissionStatus(
+                permissionType: PermissionType,
+                status: PermissionStatus
+            ) {
+                when (status) {
+                    PermissionStatus.GRANTED -> {
+                        when (permissionType) {
+                            PermissionType.CAMERA -> launchCamera = true
+                            PermissionType.GALLERY -> launchGallery = true
+                        }
+                    }
+
+                    else -> {
+                        permissionRationalDialog = true
+                    }
+                }
+            }
+        })
+
+        val cameraManager = rememberCameraManager {
+            coroutineScope.launch {
+                val bitmap = withContext(Dispatchers.Default) {
+                    it?.toImageBitmap()
+                }
+                imageBitmap = bitmap
+            }
+        }
+
+        val galleryManager = rememberGalleryManager {
+            coroutineScope.launch {
+                val bitmap = withContext(Dispatchers.Default) {
+                    it?.toImageBitmap()
+                }
+                imageBitmap = bitmap
+            }
+        }
+
+        if (imageSourceOptionDialog) {
+            ImageSourceOptionDialog(onDismissRequest = {
+                imageSourceOptionDialog = false
+            }, onGalleryRequest = {
+                imageSourceOptionDialog = false
+                launchGallery = true
+            }, onCameraRequest = {
+                imageSourceOptionDialog = false
+                launchCamera = true
+            })
+        }
+
+        if (launchGallery) {
+            if (permissionsManager.isPermissionGranted(PermissionType.GALLERY)) {
+                galleryManager.launch()
+            } else {
+                permissionsManager.askPermission(PermissionType.GALLERY)
+            }
+            launchGallery = false
+        }
+
+        if (launchCamera) {
+            if (permissionsManager.isPermissionGranted(PermissionType.CAMERA)) {
+                cameraManager.launch()
+            } else {
+                permissionsManager.askPermission(PermissionType.CAMERA)
+            }
+            launchCamera = false
+        }
+
+        if (launchSetting) {
+            permissionsManager.launchSettings()
+            launchSetting = false
+        }
+
+        if (permissionRationalDialog) {
+            AlertMessageDialog(
+                title = "Permission Required",
+                message = "To set your profile picture, please grant this permission. You can manage permissions in your device settings.",
+                positiveButtonText = "Settings",
+                negativeButtonText = "Cancel",
+                onPositiveClick = {
+                    permissionRationalDialog = false
+                    launchSetting = true
+                },
+                onNegativeClick = {
+                    permissionRationalDialog = false
+                }
+            )
+        }
+
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
+            Button(onClick = { imageSourceOptionDialog = true }) {
                 Text("Click me!")
             }
-            AnimatedVisibility(showContent) {
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource("compose-multiplatform.xml"), null)
-                    Text("Compose: $greeting")
+            Box(
+                modifier = Modifier.fillMaxSize().background(Color.DarkGray),
+                contentAlignment = Alignment.Center
+            ) {
+                if (imageBitmap != null) {
+                    Image(
+                        bitmap = imageBitmap!!,
+                        contentDescription = "Profile",
+                        contentScale = ContentScale.Crop
+                    )
                 }
             }
         }
